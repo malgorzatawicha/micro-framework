88708bc51ea0010735e6e3a249735102f62b19e9
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-03 14:16:05 +0200

    new test if there is correct exception

--- tests/SQLBuilderFactoryTest.php
+++ tests/SQLBuilderFactoryTest.php
@@ -5,9 +5,21 @@ use MW\SQLBuilderFactory;
 
 class SQLBuilderFactoryTest extends \PHPUnit_Framework_TestCase
 {
+    private function buildClass()
+    {
+        return new SQLBuilderFactory(new Connection());
+    }
     public function testClassExists()
     {
-        $class = new SQLBuilderFactory(new Connection());
-        $this->assertInstanceOf('\MW\SQLBuilderFactory', $class);
+        $this->assertInstanceOf('\MW\SQLBuilderFactory', $this->buildClass());
+    }
+
+    /**
+     * @expectedException \MW\UnrecognizedSqlQueryTypeException
+     */
+    public function testThrowsExceptionIfThereIsWrongSqlQueryType()
+    {
+        $class = $this->buildClass();
+        $class->newSqlBuilderInstance('');
     }
 }
