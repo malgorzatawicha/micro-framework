b81b52ea31c595c2533182ce962ab794d40706b0
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-03 14:16:42 +0200

    new method fetching proper query

--- src/SQLBuilderFactory.php
+++ src/SQLBuilderFactory.php
@@ -2,10 +2,31 @@
 
 class SQLBuilderFactory
 {
+    const SELECT = 'select';
+    const INSERT = 'insert';
+    const UPDATE = 'update';
+    const DELETE = 'delete';
+    
     private $connection;
     
     public function __construct(Connection $connection)
     {
         $this->connection = $connection;
+    }
+
+    public function newSqlBuilderInstance($type)
+    {
+        switch ($type) {
+            case self::SELECT: 
+                return null;
+            case self::INSERT:
+                return null;
+            case self::UPDATE:
+                return null;
+            case self::DELETE:
+                return null;
+        }
+        
+        throw new UnrecognizedSqlQueryTypeException();
     }
 }
