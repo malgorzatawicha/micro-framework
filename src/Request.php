b1e999e6813453ff6177bfd18df1134f323e3ca9
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-07 09:21:10 +0200

    new request class

---
+++ src/Request.php
@@ -0,0 +1,58 @@
+<?php namespace MW;
+
+/**
+ * Class Request
+ * @package MW
+ */
+class Request
+{
+    /**
+     * @var array
+     */
+    private $query;
+
+    /**
+     * @var array
+     */
+    private $input;
+    /**
+     * Request constructor.
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->query = $data['_GET'];
+        $this->input = $data['_POST'];
+    }
+
+    /**
+     * @return array|mixed
+     */
+    private function all()
+    {
+        return $this->query + $this->input;
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function has($name)
+    {
+        $value = $this->$name;
+        return !is_null($value);
+    }
+
+    /**
+     * @param $name
+     * @return mixed|null
+     */
+    public function __get($name)
+    {
+        $data = $this->all();
+        if (array_key_exists($name, $data)) {
+            return $data[$name];
+        }
+        return null;
+    }
+}
