8f97d41a5857fe28aead78217a40e9bb016a5826
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-08 15:27:41 +0200

    align equals signs

--- src/Controller.php
+++ src/Controller.php
@@ -7,7 +7,7 @@ class Controller
     
     public function __construct(Request $request, Response $response)
     {
-        $this->request = $request;
+        $this->request  = $request;
         $this->response = $response;
     }
     
