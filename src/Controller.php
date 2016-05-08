87de4a7c0c6ba2d8255f46ba213975be2b81064f
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-08 15:28:55 +0200

    add some comments

--- src/Controller.php
+++ src/Controller.php
@@ -1,16 +1,35 @@
 <?php namespace MW;
 
+/**
+ * Class Controller
+ * @package MW
+ */
 class Controller
 {
+    /**
+     * @var Request
+     */
     private $request;
+
+    /**
+     * @var Response
+     */
     private $response;
-    
+
+    /**
+     * Controller constructor.
+     * @param Request $request
+     * @param Response $response
+     */
     public function __construct(Request $request, Response $response)
     {
         $this->request  = $request;
         $this->response = $response;
     }
-    
+
+    /**
+     * @return Response
+     */
     public function execute()
     {
         return $this->response;    
