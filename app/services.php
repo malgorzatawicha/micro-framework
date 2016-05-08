85dfc13667b548d0474c9acebbe54222cfb72ab4
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-08 15:23:46 +0200

    add response class to dependency injection container services

--- app/services.php
+++ app/services.php
@@ -5,6 +5,10 @@ $di->addService('request', function() use($di) {
   return new \MW\Request(new \MW\RequestValue($GLOBALS));
 });
 
+$di->addService('response', function() use($di) {
+    return new \MW\Response(new \MW\Output());
+});
+
 
 $di->addService('router', function() use($di) {
     $routes = require __DIR__ . "/routes.php";
