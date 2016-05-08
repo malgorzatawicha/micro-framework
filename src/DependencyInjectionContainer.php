ba151c5acf8e5e97166577c3213c620177820f24
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-08 14:47:53 +0200

    new class: dependency injection container

---
+++ src/DependencyInjectionContainer.php
@@ -0,0 +1,57 @@
+<?php namespace MW;
+
+/**
+ * Class DependencyInjectionContainer
+ * @package MW
+ */
+class DependencyInjectionContainer
+{
+    /**
+     * @var array
+     */
+    private $services = [];
+
+    /**
+     * @param $name
+     * @param callable $callable
+     */
+    public function addService($name, callable $callable)
+    {
+        $this->services[$name] = [
+            'creationLogic' => $callable,
+            'loaded' => false
+        ];
+    }
+
+    /**
+     * @param $name
+     * @return null|Object
+     */
+    public function getService($name)
+    {
+        if (array_key_exists($name, $this->services))
+        {
+            if ($this->services['loaded'] !== false) {
+                return $this->getNewService($name);
+            }
+            return $this->services[$name]['loaded'];
+        }
+        
+        return null;
+    }
+
+    /**
+     * @param $name
+     * @return null|Object
+     */
+    public function getNewService($name)
+    {
+        if (array_key_exists($name, $this->services))
+        {
+            $this->services[$name]['loaded'] = $this->services[$name]['creationLogic']();
+            return $this->services[$name]['loaded'];
+        }
+
+        return null;
+    }
+}
