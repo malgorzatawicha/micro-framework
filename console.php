6e5cfe745c5bedd25762de883ef69b6258dc99c5
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-14 20:37:46 +0200

    change console commands invocation. new test for help command

--- console.php
+++ console.php
@@ -2,12 +2,5 @@
 
 require_once 'vendor/autoload.php';
 
-$configs = require 'app/config/db.php';
-$connectionFactory = new \MW\Connection\ConnectionFactory(
-    new \MW\Connection\PDOFactory(),
-    require 'app/config/db.php'
-);
-
-$sqlBuilderFactory = new \MW\SQLBuilderFactory($connectionFactory->getConnection('default'));
-$dispatcher = new \MW\Commands\CommandDispatcher($sqlBuilderFactory, array_slice($argv, 1));
+$dispatcher = new \MW\Commands\CommandDispatcher(require 'app/commandServices.php', array_slice($argv, 1));
 $dispatcher->dispatch();
---
+++ tests/Commands/HelpCommandTest.php
@@ -0,0 +1,47 @@
+<?php namespace Tests\Commands;
+
+use MW\Commands\HelpCommand;
+use MW\DirectoryIteratorFactory;
+use MW\Output;
+use Tests\BaseTest;
+
+class HelpCommandTest extends BaseTest
+{
+    private function getDirectoryIteratorFactoryMock()
+    {
+        return $this->getMockBuilder('\MW\DirectoryIteratorFactory')
+            ->getMock();
+    }
+    public function testClassExists()
+    {
+        $class = new HelpCommand($this->getDirectoryIteratorFactoryMock(), $this->getOutputMock(), []);
+        $this->assertInstanceOf('\MW\Commands\HelpCommand', $class);
+    }
+
+    public function testExecutingCommandWithEmptySearchPaths()
+    {
+        $class = new HelpCommand($this->getDirectoryIteratorFactoryMock(), $this->getOutputMock(), []);
+        $class->execute([]);
+    }
+
+    public function testExecutingCommandWithDummySearchPaths()
+    {
+       
+        $mock = $this->getDirectoryIteratorFactoryMock();
+        $mock->expects($this->once())->method('getPhpDirectoryIterator')->with('dummy')->willReturn([new \SplFileInfo('test')]);
+        $class = new HelpCommand($mock, $this->getOutputMock(), ['dummy']);
+        $class->execute([]);
+    }
+
+    public function testExecutingCommandWithCommandInSearchPath()
+    {
+
+        $mock = $this->getDirectoryIteratorFactoryMock();
+        $mock->expects($this->once())->method('getPhpDirectoryIterator')->with('dummy')->willReturn([new \SplFileInfo('TestCommand.php')]);
+        $outputMock = $this->getOutputMock();
+        $outputMock->expects($this->once())->method('content')->with("test\n");
+        
+        $class = new HelpCommand($mock, $outputMock, ['dummy']);
+        $class->execute([]);
+    }
+}
