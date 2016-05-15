241aa88608c8272170fa9c7016363f0e6d300e64
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-15 08:53:19 +0200

    fix test - make command use migration model instead of sql builder factory

--- tests/Commands/Migrate/MakeCommandTest.php
+++ tests/Commands/Migrate/MakeCommandTest.php
@@ -1,24 +1,23 @@
 <?php namespace Tests\Commands\Migrate;
 
 use MW\Commands\Migrate\MakeCommand;
-use MW\SQLBuilderFactory;
 use Tests\BaseTest;
 
 class MakeCommandTest extends BaseTest
 {
     public function testClassExists()
     {
-        $class = new MakeCommand($this->getSqlBuilderFactoryMock());
+        $class = new MakeCommand($this->getMigrationModelMock());
         $this->assertInstanceOf('\MW\Commands\Migrate\MakeCommand', $class); 
     }
 
     public function testExecuting()
     {
-        $sqlBuilderFactoryMock = $this->getSqlBuilderFactoryMock();
-        $sqlBuilderFactoryMock->expects($this->once())->method('newSqlBuilderInstance')
-            ->with(SQLBuilderFactory::CUSTOM)->will($this->returnValue($this->getCustomQueryMock('CREATE TABLE migrations(migration int(11))')));
+        $modelMock = $this->getMigrationModelMock();
+        $modelMock->expects($this->once())->method('executeCustomQuery')
+            ->with('CREATE TABLE migrations(migration int(11))')->willReturn(true);
         
-        $class = new MakeCommand($sqlBuilderFactoryMock);
+        $class = new MakeCommand($modelMock);
 
         $class->execute();
     }
