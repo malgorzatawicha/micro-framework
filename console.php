927bf72cd909693423de81bb30db2c020ca643a2
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-05-15 10:00:43 +0200

    use new format of dispatching commands

--- console.php
+++ console.php
@@ -2,5 +2,13 @@
 
 require_once 'vendor/autoload.php';
 
-$dispatcher = new \MW\Commands\CommandDispatcher(require 'app/commandServices.php', array_slice($argv, 1));
+$dispatcher = new \MW\Commands\CommandDispatcher(
+    require 'app/commandServices.php',
+    [
+        \MW\Commands\Migrate\MakeCommand::class,
+        \MW\Commands\MigrateCommand::class,
+        \MW\Commands\Migrate\RollbackCommand::class,
+        \MW\Commands\HelpCommand::class
+    ],
+    array_slice($argv, 1));
 $dispatcher->dispatch();
