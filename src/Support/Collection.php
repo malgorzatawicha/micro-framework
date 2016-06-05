b338e8e5e5f7855cace5364be9193bcb12fbfba0
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-06-05 16:40:31 +0200

    implement some array access methods in collection class

--- src/Support/Collection.php
+++ src/Support/Collection.php
@@ -1,6 +1,31 @@
 <?php namespace MW\Support;
 
-class Collection
+class Collection implements \ArrayAccess
 {
+    private $elements;
     
+    public function __construct(array $elements = [])
+    {
+        $this->elements = $elements;
+    }
+
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->elements);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->elements[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $this->elements[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->elements[$offset]);
+    }
 }
--- tests/Support/CollectionTest.php
+++ tests/Support/CollectionTest.php
@@ -10,4 +10,12 @@ class CollectionTest extends BaseTest
         $class = new Collection();
         $this->assertInstanceOf('\MW\Support\Collection', $class);
     }
+
+    public function testCanAccessCollectionAsSimpleArray()
+    {
+        $class = new Collection([1, 2, 3]);
+        $this->assertEquals(1, $class[0]);
+        $this->assertEquals(2, $class[1]);
+        $this->assertEquals(3, $class[2]);
+    }
 }
